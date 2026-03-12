19fab9787b5334877d3d236f3a8cefa25fa2b900
IlyaManishin <manishin06@gmail.com>
2026-03-12 23:30:49 +0700

    add alu tests

--- verilog/sim/test_cocotb.py
+++
@@ -1,8 +0,0 @@
-# cocotb-тест
-import cocotb
-from cocotb.clock import Clock
-from cocotb.triggers import RisingEdge, Timer
-
-@cocotb.test()
-async def test_rf_read_write(dut):
-    cocotb.start_soon(Clock(dut.clk, 10, units="ns").start())
