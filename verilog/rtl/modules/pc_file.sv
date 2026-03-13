6ebfb51cfe25dd7189644a6d7d3d66b12065eb6f
Nikolay56615 <nikolay56615@mail.ru>
2026-03-13 19:48:19 +0700

    fix PC

--- verilog/rtl/modules/pc_file.sv
+++ verilog/rtl/modules/pc_file.sv
@@ -1,35 +1,36 @@
-`timescale 1ns / 1ps
-import core_base_pkg::*;
+module pc_file #(
+    parameter int  XLEN = core_base_pkg::XLEN,
+    parameter logic [XLEN-1:0] RESET_PC = core_base_pkg::RESET_PC,
+    parameter logic [XLEN-1:0] STEP     = core_base_pkg::PC_STEP
+) (
+    input  logic            i_clk,
+    input  logic            i_rst,
 
-module program_counter 
-#(
-    parameter int WIDTH = XLEN,
-    parameter logic [WIDTH-1:0] PC_START_ADDR = '0,
-    parameter logic [WIDTH-1:0] PC_INCR = 16'd4
-)(
-    input  logic clk,
-    input  logic rst,
-    input  logic br_taken,
-    input  logic [WIDTH-1:0] pc_br,
-    output logic [WIDTH-1:0] pc
+    // Управление
+    input  logic            i_hold,
+    input  logic            i_inc,
+    input  logic            i_load,
+    input  logic [XLEN-1:0] i_load_data,
+
+    // Состояние
+    output logic [XLEN-1:0] o_pc,
+    output logic [XLEN-1:0] o_pc_next_inc,
+    output logic            o_unaligned
 );
 
-    logic [WIDTH-1:0] assign_pc;
-
-    always_comb begin
-        if (br_taken) begin
-            assign_pc = pc_br;
-        end else begin
-            assign_pc = pc + PC_INCR;
+    always_ff @(posedge i_clk) begin
+        if (i_rst) begin
+            o_pc <= RESET_PC;
+        end else if (!i_hold) begin
+            if (i_load) begin
+                o_pc <= i_load_data;
+            end else if (i_inc) begin
+                o_pc <= o_pc + STEP;
+            end
         end
     end
 
-    always_ff @(posedge clk) begin
-        if (rst) begin
-            pc <= PC_START_ADDR;
-        end else begin
-            pc <= assign_pc;
-        end
-    end
+    assign o_pc_next_inc = o_pc + STEP;
+    assign o_unaligned   = o_pc[0];
 
-endmodule : program_counter
+endmodule
