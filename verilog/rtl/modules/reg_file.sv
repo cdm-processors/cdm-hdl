68eaed127392e4ab5c068fe556fdb0d1b7e6b08a
IlyaManishin <manishin06@gmail.com>
2026-04-03 18:29:41 +0700

    fix reg file warnings/compille errors

--- verilog/rtl/modules/reg_file.sv
+++ verilog/rtl/modules/reg_file.sv
@@ -1,9 +1,7 @@
-`include "core_base.svh"
-
-module register_file_m import core_base_pkg::*;
+module register_file_m
 #(
-    parameter XLEN = XLEN,
-    parameter REG_CNT = REG_CNT,
+    parameter XLEN = 16,
+    parameter REG_CNT = 8,
 
     localparam REG_ADDR_W = ($clog2(REG_CNT))
 ) (
@@ -21,7 +19,7 @@ module register_file_m import core_base_pkg::*;
     input logic we
 );
 
-  logic [XLEN-1:0] regFile[0:REG_CNT-1] = '{default: 0};  // or no init?
+  logic [XLEN-1:0] regFile[0:REG_CNT-1];
 
   function automatic logic [XLEN-1:0] rs(input [REG_ADDR_W-1:0] rsi);
     rs = regFile[rsi];
