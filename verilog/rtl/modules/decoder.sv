600fd48a16d4ff7b8ec9532348ba146a4f77427b
IlyaManishin <manishin06@gmail.com>
2026-04-09 19:28:01 +0700

    final fixes, no critical warnings

--- verilog/rtl/modules/decoder.sv
+++ verilog/rtl/modules/decoder.sv
@@ -5,7 +5,7 @@
 module decoder import core_base_pkg::*;
 (
     input logic [XLEN-1:0] instr,
-    input u_phase_t phase; 
+    input u_phase_t phase,
     input logic [ 3:0] CVZN,
 
     output u_addr_t ucode_addr,
@@ -18,8 +18,8 @@ module decoder import core_base_pkg::*;
     output logic [2:0] alu_op_type,
     output logic [2:0] shamt,
 
-    output logic [5:0] imm6_d,
-    output logic [8:0] imm9_d,
+    output logic [5:0] imm6,
+    output logic [8:0] imm9,
 
     output flag_t imm6_flag,
     output flag_t carry_flag,
@@ -46,7 +46,7 @@ module decoder import core_base_pkg::*;
 //____________________ALU______________________
     wire [2:0] alu_op_d0 = instr[8:6];
     wire [2:0] alu_op_d1 = instr[11:9];
-    assign shamt = inst[8:6];
+    assign shamt = instr[8:6];
 
 
 //____________________DATA______________________
@@ -54,13 +54,13 @@ module decoder import core_base_pkg::*;
     wire [2:0] rs1_d = instr[8:6];
     wire [2:0] rd_d  = instr[2:0];
 
-    assign imm6_d = instr[8:3];
-    assign imm9_d = instr[8:0];
+    assign imm6 = instr[8:3];
+    assign imm9 = instr[8:0];
 
 
 //__________________OP_PARSING______________________
     logic op0_d        = (inst_type == 0) && (postf == 2'b00);
-    logic br_abs_d_d   = (inst_type == 0) && (postf == 2'b01);
+    logic br_abs_d     = (inst_type == 0) && (postf == 2'b01);
     logic shifts_d     = (inst_type == 0) && (postf[0] == 1);
     logic op1_d        = (inst_type == 1);
     logic op2_d        = (inst_type == 2) && (postf == 2'b00);
@@ -95,15 +95,15 @@ module decoder import core_base_pkg::*;
 
 
 //__________________SET_FLAGS______________________
-    logic [2:0] alu_op_type_raw = {shifts, alu2, alu3 || alu3_ind};
+    logic [2:0] alu_op_type_raw = {shifts_d, alu2_d, alu3_d || alu3_ind_d};
     assign alu_op_type = |alu_op_type_raw ? alu_op_type_raw : 3'b001;
 
 
     assign imm6_flag = imm6_d;
     assign is_int = imm9_d && op_type_d3[3:1] == 0;
     assign is_branch = br_abs_d || br_rel_n_d || br_rel_p_d;
-    assign carry_flag =    (alu3   && (_alu_func == 3'd5 || _alu_func == 3'd7))
-                         || (shifts && (_alu_func == 3'd5 || _alu_func == 3'd6));
+    assign carry_flag =    (alu3_d   && (_alu_func == 3'd5 || _alu_func == 3'd7))
+                        || (shifts_d && (_alu_func == 3'd5 || _alu_func == 3'd6));
 
 //___________________BRANCH_________________________
     logic br_go;
@@ -129,10 +129,10 @@ module decoder import core_base_pkg::*;
         else if (br_abs_nop) direct_op_type = 4'h6;
         else if (br_rel_n) direct_op_type = 4'h5;
         else if (br_rel_p) direct_op_type = 4'h4;
-        else if (alu3) direct_op_type = 4'h3;
-        else if (alu3_ind) direct_op_type = 4'h2;
-        else if (shifts || alu2) direct_op_type = 4'h1;
-        else if (br_abs) direct_op_type = 4'h0;
+        else if (alu3_d) direct_op_type = 4'h3;
+        else if (alu3_ind_d) direct_op_type = 4'h2;
+        else if (shifts_d || alu2_d) direct_op_type = 4'h1;
+        else if (br_abs_d) direct_op_type = 4'h0;
         else direct_op_type = 4'h8;
     end
 
@@ -145,7 +145,7 @@ module decoder import core_base_pkg::*;
         UCODE_IMM9   = 3'h5,
         UCODE_MEM3   = 3'h6,
         UCODE_DIRECT = 3'h7
-    } ucode_sel_e;
+    } ucode_sel_e; // make extern to global
 
     always_comb begin
         if (direct_op_type != 4'h8) ucode_addr = {phase, UCODE_DIRECT,  direct_op_type};
@@ -156,6 +156,7 @@ module decoder import core_base_pkg::*;
         else if (op2_d)             ucode_addr = {phase, UCODE_OP2,     op_type_d2};
         else if (op1_d)             ucode_addr = {phase, UCODE_OP1,     op_type_d1};
         else if (op0_d)             ucode_addr = {phase, UCODE_OP0,     op_type_d0};
+        else                        ucode_addr = 'x;
     end
 
 endmodule
