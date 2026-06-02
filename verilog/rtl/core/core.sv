b952ac4ae11498ab9fa996809843223021cdfb46
IlyaManishin <manishin06@gmail.com>
2026-06-02 14:45:49 +0700

    add memory api

--- verilog/rtl/core/core.sv
+++ verilog/rtl/core/core.sv
@@ -31,36 +31,8 @@ module core
   );
 
   // ===================== MEMORY ======================
-
-  //individual clk for load, store data in mem.
-  logic rev_clk;
-  assign rev_clk = ~clk;
-
-  rams_tdp_rf_rf #(
-      .WIDTH (XLEN),
-      .ADDR_W(MEM_ADDR_WIDTH)
-  ) u_rams_tdp_rf_rf (
-
-      // _____INSTRUCTIONS_____
-      .clka(clk),
-
-      .ena(ena),
-      .wea(0),
-
-      .addra(next_pc[MEM_ADDR_WIDTH-1:0]),
-      .dia  (0),
-      .doa  (instr),
-
-      // ______DATA______
-      .clkb(rev_clk),
-
-      .enb(enb),
-      .web(web),
-
-      .addrb(addrb),
-      .dib  (dib),
-      .dob  (dob)
-  );
+  //instance of memory
+ 
 
 
-endmodule  //core
+endmodule  // core
